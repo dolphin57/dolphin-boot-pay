962bd1cace389b913e43e8f79dd5d17866ecc6d6
Eric Liang <120444121@hdjt.hdad.local>
2020-09-06 19:53:48 +0800

    初始化SpringBoot Pay项目

---
+++ src/main/java/io/dolphin/pay/DolphinBootPayApplication.java
@@ -0,0 +1,13 @@
+package io.dolphin.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinBootPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolphinBootPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dolphin/pay/DolphinBootPayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dolphin.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolphinBootPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
